392a20bfe39184904a8b7eb4cc48b884ffe7dd6e
Virajpatel4305 <virajpatelmi4305@gmail.com>
2025-05-22 12:22:59 +0000

    SecondApplet

---
+++ firstapplet.java
@@ -0,0 +1,7 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class firstapplet extends Applet{
+    public void paint(Graphics g){
+        g.drawsString("Hello, World!",20 ,20);
+    }
+}
